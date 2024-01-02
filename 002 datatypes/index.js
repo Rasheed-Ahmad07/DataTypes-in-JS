0a11d0a425e9ab815dfcca6d7c4e1364f127ca25
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-01-02 23:12:56 +0500

    Add files via upload

---
+++ 002 datatypes/index.js
@@ -0,0 +1,81 @@
+
+//string ..... double qute  
+//number
+//boolean
+//array
+//object
+//null
+//undefined
+//big int
+//symbol
+
+//number 
+// var x=4;
+// console.log(typeof x);
+
+
+// bigint
+// let x=(2*10)^100;
+// console.log(typeof x);
+
+//string
+// var x="rasheed ahmad";
+// var x=`sadeeq`;
+// var x="4";
+// var x= ""; //..empty string
+// console.log(typeof x);
+
+//boolean
+// var rasheed =false;
+// var rasheed =true;
+// console.log(typeof rasheed);
+
+// let x=3;
+// let y=5;
+// let z= x>y;
+// console.log(z);
+// console.log(typeof z);
+
+
+// null
+// let x= null;
+// console.log(x);
+// console.log(typeof x);
+
+// value = null;
+// if( value === null){
+//     console.log("null");
+// }else{
+//     console.log("not null value");
+// }
+
+// undefined/
+//  let x   ....... declaration
+// let x=4  ....... assign/ define
+
+// let x;
+// console.log(typeof x)
+
+
+
+//object  {}
+//key value pairs
+
+// let rasheed={
+// height :"5feet",
+// dob: 2000,
+// color : "gray"
+
+// }
+
+// console.log(rasheed);
+// console.log(typeof rasheed);
+
+
+
+// array [] 
+// let me=[2,"ali",6];
+// console.log(me);
+// console.log(typeof me);
+
+
